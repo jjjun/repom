6f77e82df7bd7b9097ab0a44983369f890c15106
jj@jjjun.net <soft.management943@gmail.com>
2025-11-19 17:12:13 +0900

    update

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,12 +4,10 @@ import logging
 from repom.testing import create_test_fixtures
 
 
-def pytest_configure(config):
-    os.environ['EXEC_ENV'] = 'test'
-    # CONFIG_HOOK を空文字列にすると py-mine の設定フックが無効化される
-    # つまりは、このパッケージ単体で動かしたい場合には、CONFIG_HOOK を空にする
-    os.environ['CONFIG_HOOK'] = ''
+os.environ['EXEC_ENV'] = 'test'
 
+
+def pytest_configure(config):
     # -vv オプション時のみデバッグログを有効化
     if config.option.verbose >= 2:
         logging.basicConfig(
